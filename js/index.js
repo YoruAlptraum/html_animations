bfa04d12b96be07e892f00f7388a6fcf287e6cb9
Thiago Daisuke Ozawa <72313676+YoruAlptraum@users.noreply.github.com>
2023-05-09 16:58:44 -0300

    reversed gradient
    the gradient was brighter going down, now it gets brighter going up

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ rgb = [157, 78, 221]
 /* the percentage to increase for each gradient */
 var percent = (1/(links.length+1));
 
-for(let i = 0; i <= links.length; i++){
+for(let i = (links.length-1); i >= 0; i--){
     rgb = rgb.map(rgb => lerp(rgb, percent));
     links[i].style.backgroundColor = `rgb(${rgb[0]} ${rgb[1]} ${rgb[2]})`;
 }
