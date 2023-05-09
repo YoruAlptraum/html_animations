05a2f10533404a3348d22918c9f797f6641c176e
Thiago Daisuke Ozawa <72313676+YoruAlptraum@users.noreply.github.com>
2023-05-09 16:35:36 -0300

    Simple grad applied to index

--- js/index.js
+++ js/index.js
@@ -3,11 +3,13 @@ console.log(links.length)
 
 rgb = [157, 78, 221]
 
-for(let i = 1; i < links.length; i++){
-    var percent = 1/links.length * i;
-    for(let j = 0; j < rgb.length; j++){
+for(let i = 0; i <= links.length; i++){
+    var percent = 1/(links.length+1) * (i+1);
+/*     for(let j = 0; j < rgb.length; j++){
         console.log(lerp(rgb[j], percent));
-    }
+    } */
+    rgb = rgb.map(rgb => lerp(rgb, percent));
+    links[i].style.backgroundColor = `rgb(${rgb[0]} ${rgb[1]} ${rgb[2]})`;
 }
 
 function lerp(min, percent){
